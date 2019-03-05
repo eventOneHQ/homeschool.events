a0884af09aa5aff6511b33658e78286f8a3c2999
Noah Prail <nprail@event1.io>
2019-03-05 16:55:11 -0500

    chore: Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <div id="app">
+  <div id="app" class="d-flex flex-column h-100">
     <div>
       <b-navbar toggleable="lg" type="dark" variant="success">
         <b-navbar-brand to="/">homeschool.events</b-navbar-brand>
 
-        <b-navbar-toggle target="nav_collapse" />
+        <b-navbar-toggle target="nav_collapse"/>
 
         <b-collapse is-nav id="nav_collapse">
           <b-navbar-nav>
@@ -15,10 +15,21 @@
       </b-navbar>
     </div>
 
-    <router-view/>
+    <main role="main" class="flex-shrink-0">
+      <router-view/>
+    </main>
+
+    <footer class="footer mt-auto py-3">
+      <div class="container">
+        <span class="text-muted">Created by <a href="https://e1.lc/tAfSUQ">eventOne</a> - <a href="https://github.com/eventOneHQ/homeschool.events">Open Source</a></span>
+        <span class="text-muted float-right">&copy; {{new Date().getFullYear()}} eventOne, Inc.</span>
+      </div>
+    </footer>
   </div>
 </template>
 
 <style>
-
+.footer {
+  background-color: #f5f5f5;
+}
 </style>
